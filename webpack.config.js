9400c670b0ed89f39e755042a04c4644dbd26a10
cmarioep <cmescandonpardo@gmail.com>
2021-07-17 14:52:33 -0500

    Babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,18 @@ module.exports = {
   resolve: {
     extensions: [".js"],
   },
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader
+        test: /\.js$/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: "babel-loader"
+        },
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
